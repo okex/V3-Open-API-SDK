659d36bfdb9d41efb644ff229381af18ce17bfc6
MigunovAlex <migunovalex84@gmail.com>
2020-04-12 11:25:25 -0400

    Java 13 Migration: changed Instant to LocalDateTime for formatting in proper pattern

--- okex-java-sdk-api/src/test/java/com/okcoin/commons/okex/open/api/test/futures/FuturesTradeAPITests.java
+++ okex-java-sdk-api/src/test/java/com/okcoin/commons/okex/open/api/test/futures/FuturesTradeAPITests.java
@@ -15,7 +15,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.util.resources.cldr.mg.LocaleNames_mg;
 
 import java.util.ArrayList;
 import java.util.Date;
